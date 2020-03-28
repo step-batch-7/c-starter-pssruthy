765efa302df7824685a23be3c1ccf8eb52f0efde
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-03-28 22:31:22 +0530

    Added isOdd function to determines the given number is odd or not

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int isEven(int);
+int isOdd(int);
 
 int isEven(int num){
   return num % 2 == 0;
+}
+
+int isOdd(int num){
+  return isEven(num) == 0;
 }
 
 int main(void){
@@ -15,7 +20,7 @@ int main(void){
 
   printf("Enter a number to check odd : ");
   scanf("%d", &num);
-  printf("%s\n" ,isEven(num)?"not odd":"odd");
+  printf("%s\n" ,isOdd(num)?"odd":"not odd");
 
   return 0;
 }
