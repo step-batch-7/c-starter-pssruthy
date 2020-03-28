d908019d07b7a024f02d8f0470259c4220bd61b0
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-03-28 23:57:46 +0530

    Added GCD function to find greatest common divisor of two given number

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -4,6 +4,7 @@ int isEven(int);
 int isOdd(int);
 unsigned int square(int);
 int cube(int);
+int GCD(int, int);
 
 int isEven(int num){
   return num % 2 == 0;
@@ -21,25 +22,41 @@ int cube(int num){
   return square(num) * num;
 }
 
+int GCD(int num1, int num2){
+
+  int min = num1 > num2 ? num2 : num1;
+
+  for(int min = num1; min >= 1; min--){
+    if(num1 % min == 0 && num2 % min == 0){
+      return min;
+    }
+  }
+}
+
 int main(void){
 
   int num;
-  
+
   printf("Enter a number to check even : ");
   scanf("%d", &num);
-  printf("%s\n" ,isEven(num)?"even":"not even");
+  printf("%s\n\n" ,isEven(num)?"even":"not even");
 
   printf("Enter a number to check odd : ");
   scanf("%d", &num);
-  printf("%s\n" ,isOdd(num)?"odd":"not odd");
+  printf("%s\n\n" ,isOdd(num)?"odd":"not odd");
 
   printf("Enter a number to determine square : ");
   scanf("%d", &num);
-  printf("%u\n" ,square(num));
+  printf("%u\n\n" ,square(num));
 
   printf("Enter a number to determine cube : ");
   scanf("%d", &num);
-  printf("%d\n" ,cube(num));
+  printf("%d\n\n" ,cube(num));
+
+  int num1, num2;
+  printf("Enter two numbers to find the greatest common divisor : ");
+  scanf("%d %d", &num1, &num2);
+  printf("GCD : %d\n\n" ,GCD(num1, num2));
 
   return 0;
 }
