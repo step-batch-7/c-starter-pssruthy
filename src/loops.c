6a911abd37c1e67063e3fe7b756a6942710d30b7
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-03-30 13:10:55 +0530

    Added feature to print fibonacci series

--- src/loops.c
+++ src/loops.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 long factorial(int);
+void fibonacci(int);
 
 long factorial(int number){
   long factorial = 1;
@@ -10,19 +11,39 @@ long factorial(int number){
   return factorial;
 }
 
+void fibonacci(int count){
+  int current_term = 0, next_term=1;
+
+  while(count > 0) {
+    printf("%d\n", current_term);
+    next_term = current_term + next_term;
+    current_term = next_term - current_term;
+    count--;
+  }
+}
+
 int main(void ){
 
   int choice , number;
 
-  printf("Choose a number \n1. Factorial\n");
+  printf("Choose a number \n1. Factorial\n2. Fibonacci\nChoice : ");
   scanf("%d" ,&choice);
 
   switch (choice)
   {
   case 1:
+    printf("\n---Factorail---\n");
     printf("Enter the number : ");
     scanf("%d", &number);
     printf("Factorial : %ld\n", factorial(number));
+    break;
+
+  case 2:
+    printf("\n---Fibonacci---\n");
+    printf("Enter the number : ");
+    scanf("%d", &number);
+    printf("Fibonacci series : \n");
+    fibonacci(number);
     break;
   
   default: printf("Wrong choice");
