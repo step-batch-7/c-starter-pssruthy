2dc7b9e26436a0d291e7610bae3477c1b7fceafe
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-03-30 14:07:55 +0530

    Added feature to print multiplication table

--- src/loops.c
+++ src/loops.c
@@ -4,6 +4,7 @@ long factorial(int);
 void print_fibonacci(int);
 void print_odd_number_series(long);
 void print_even_number_series(long);
+void print_multiplication_table(long, long);
 
 long factorial(int number){
   long factorial = 1;
@@ -40,12 +41,20 @@ void print_even_number_series(long limit){
   }
 }
 
+void print_multiplication_table(long number, long limit){
+  for (long count = 1; count <= limit; count++) 
+  {
+    printf("%ld * %ld = %ld\n", count, number, count * number);
+  }
+}
+
+
 int main(void ){
 
   int choice , number;
   long limit;
 
-  printf("Choose a number \n1. Factorial\n2. Fibonacci\n3. Odd number series\n4. Even number series\nChoice : ");
+  printf("Choose a number \n1. Factorial\n2. Fibonacci\n3. Odd number series\n4. Even number series\n5. Multiplicaton table\nChoice : ");
   scanf("%d" ,&choice);
 
   switch (choice)
@@ -79,6 +88,17 @@ int main(void ){
     scanf("%ld", &limit);
     printf("Even Number Series : \n");
     print_even_number_series(limit);
+    break;
+
+    case 5:
+    printf("\n---Multiplication table---\n");
+    printf("Enter the number : ");
+    long num,count;
+    scanf("%ld", &num);
+    printf("Enter the count : ");
+    scanf("%ld", &count);
+    printf("Even Number Series : \n");
+    print_multiplication_table(num,count);
     break;
   
   default: printf("Wrong choice");
